afefc3beff26b53c323ffb42b4c278f30c60ac44
Yasmini <40608901+yasminicau@users.noreply.github.com>
2018-07-10 13:29:29 -0300

    Sessão Login
    Arquivo responsavel por verificar se o usuario existe no banco de dados e redicenionar para a pagina do usuario. ou retorna um erro caso o usuario não exista

---
+++ login.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once 'conexaoBD.php';
+
+$senha= $_POST['senhaLogin'];
+$nome = $_POST['nomeLogin']; 
+
+$consultar = "SELECT * FROM usuario WHERE senhaLogin='$senha' AND nomeLogin='$nome' ";
+
+$result = mysqli_query($connect, $consultar);
+
+if(!mysqli_num_rows($result)==1):
+   header('Location: recuperarSenha.php');
+endif;
+
+   $id = mysqli_fetch_array($result); 
+   $_SESSION[logado]= true;
+   $_SESSION[id]=$id['id'];
+   header('Location: userSession.php');
+endif;
+
+?>
